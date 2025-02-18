e74903de3b183266fedd1000a49f30b948f1eaf9
Alex <alexandre.barbez@gmail.com>
2025-02-18 17:47:37 +0100

    Ajout de quelques exos sur les stream !

--- src/main/java/org/abrbz/Main.java
+++ src/main/java/org/abrbz/Main.java
@@ -1,14 +1,48 @@
 package org.abrbz;
 
+import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         PersonneGenerateur personneGenerateur = new PersonneGenerateur();
-        List<Personne> personnes = personneGenerateur.generate(10);
+        List<Personne> personnes = personneGenerateur.generate(100);
+
+        //toutes les personnes majeures
+        ArrayList<Personne> personnesmajeures = personnes
+                .stream()
+                .filter(personne -> personne.age() >= 18)
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println("Majeurs : " + personnesmajeures);
+
+        //toutes les personnes dont le prénom commence par un A
+        ArrayList<Personne> personnesEnA = personnes
+                .stream()
+                .filter(personne -> personne.prenom().startsWith("A"))
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println("Prenoms en A : " + personnesEnA);
+
+        //toutes les personnes avec un prénom composé
+        ArrayList<Personne> personnesPrenomCompose = personnes
+                .stream()
+                .filter(personne -> personne.prenom().contains("-"))
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println("prénoms composés : " + personnesPrenomCompose);
+
+        //la moyenne des ages
+        double moyenne = personnes.stream().mapToInt(Personne::age).average().orElse(0);
+        System.out.println("Moyenne des ages : " + moyenne);
+
+        //la moyenne des ages par prénoms
+        Map<String, IntSummaryStatistics> moyenneAgeparPrenom = personnes
+                .stream()
+                .collect(Collectors.groupingBy(Personne::prenom, Collectors.summarizingInt(Personne::age)));
+
+        moyenneAgeparPrenom.forEach( (key, value) -> System.out.println(key + " : moyenne=" + value.getAverage() + " : total=" + value.getCount() ));
 
     }
 }
